4a920dffffee66a2ab76a62c8992beb83074738a
Raghavendra-J-R <jrragh@gmail.com>
2025-04-16 12:36:04 +0530

    added print statement

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/operation/verify', methods=['GET'])
+
+@app.route("/operation/verify", methods=["GET"])
 def verify_operation():
+    print("Smooth running in progress")
     return jsonify({"status": "Running smoothly"})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
